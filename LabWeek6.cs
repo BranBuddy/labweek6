5ba146de3d885c74efe5db4a6a1eb92e40e5aa4d
IstvanW05 <163757232+IstvanW05@users.noreply.github.com>
2025-02-13 12:20:38 -0500

    YES!!

--- LabWeek6.cs
+++ LabWeek6.cs
@@ -6,7 +6,7 @@ public class LabWeek6 : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public int sizeOfForest = Random.Range(5, 15);
+    public int sizeOfForest;
     public int stonesrequired;
     public GameObject[] tress;
     public GameObject[] stones;
@@ -28,39 +28,48 @@ public class LabWeek6 : MonoBehaviour
         renderer.material.color = Color.red;
 
         plane.transform.localScale = new Vector3(10,10,10);
+
+        // Possibly smaller?
     }
 
     void CreateForest()
     {
-        for (int i = 0; i < sizeOfForest; i++)
+        for (int i = 1; i < sizeOfForest; i++)
         {
-             GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
 
-                    Renderer renderer = cylinder.GetComponent<Renderer>();
-                    renderer.material = new Material(Shader.Find("Standard"));
-                    renderer.material.color = Color.green;
+            Renderer renderer = cylinder.GetComponent<Renderer>();
+            renderer.material = new Material(Shader.Find("Standard"));
+            renderer.material.color = Color.green;
 
-                    cylinder.transform.localScale = new Vector3(Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax));
-                    cylinder.transform.position = new Vector3(Random.Range(min, max), Random.Range(0, max), Random.Range(min, max));
+            cylinder.transform.position = new Vector3(Random.Range(min, max), Random.Range(0, max), Random.Range(min, max));
+            cylinder.transform.localScale = new Vector3(Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax));
+            
 
-           Debug.Log("Object Created" + i);
+            Debug.Log("Object Created" + i);
+
+            /*
+                We need to alternate between colors
+                Change sizes and postions
+                Spawns Random number of objects
+                Spawn on 1 side of plan
+            */
         }
 
        
-        
-
     }
 
 
     void CreatePyramid()
     {
-
+        // JUST DO IT!
     }
    
     void Start()
     {
         CreatePlan();
         CreateForest();
+
     }
 
     // Update is called once per frame
