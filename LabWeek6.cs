ade2b4e8992a3e3c6e9eda9f473f8c23a1214a92
IstvanW05 <163757232+IstvanW05@users.noreply.github.com>
2025-02-13 11:50:29 -0500

    Script

---
+++ LabWeek6.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabWeek6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int sizeOfForest = 15;
+    public int stonesrequired;
+    public GameObject[] tress;
+    public GameObject[] stones;
+
+    public int min = -3;
+    public int max = 0;
+    public float scaleMin = 0.1f;
+    public float scaleMax = 1.0f;
+    // Creates plane
+    void CreatePlan()
+    {
+        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+        Renderer renderer = plane.GetComponent<Renderer>();
+        renderer.material = new Material(Shader.Find("Standard"));
+        renderer.material.color = Color.red;
+
+        plane.transform.localScale = new Vector3(10,10,10);
+    }
+
+    void CreateForest()
+    {
+        //for (int i = 0; i == sizeOfForest; i++)
+        
+             GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+
+                    Renderer renderer = cylinder.GetComponent<Renderer>();
+                    renderer.material = new Material(Shader.Find("Standard"));
+                    renderer.material.color = Color.green;
+
+                    cylinder.transform.localScale = new Vector3(Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax), Random.Range(scaleMin, scaleMax));
+                    cylinder.transform.position = new Vector3(Random.Range(min, max), Random.Range(0, max), Random.Range(min, max));
+
+           // Debug.Log("Object Created" + i);
+        
+
+       
+        
+
+    }
+
+
+    void CreatePyramid()
+    {
+
+    }
+   
+    void Start()
+    {
+        CreatePlan();
+        CreateForest();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
